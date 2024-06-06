151e67ec0c45e2c14e7a2d3ca315a04cb7c56dfb
Jinghao Tu <jinghaotu213@jht213.com>
2024-06-06 13:28:14 +0800

    move backend to another repository for a submodule.

--- backend/app/src/main/java/backend/trip/TripOption.java
+++
@@ -1,5 +0,0 @@
-package backend.trip;
-
-public enum TripOption {
-    roadTrip, groupTrip, adult, men, women
-}
--- backend/app/src/main/java/backend/trip/TripRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.trip;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TripRepository extends JpaRepository<Trip, Long> {
-    Trip findByTitle(String title);
-}
